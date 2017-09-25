5d7c782a1f71dff9ec1f6ea8968df9021d7e9cd3
Chris Troutner <chris.troutner@gmail.com>
2017-09-25 16:57:25 -0700

    Adding in Admin LTE components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app" style="border: 1px solid black">
     <img src="./assets/logo.png">
+    <left-menu></left-menu>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  
+  import LeftMenu from './components/LeftMenu'
+  
+  export default {
+    name: 'app',
+    components: {
+      LeftMenu
+    },
+  }
 </script>
 
 <style>
---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello" style="border: 1px solid red;">
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'left-menu',
+  data () {
+    return {
+      msg: 'This is the left menu'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
