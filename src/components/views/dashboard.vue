ff8ef24d95d0b96e0343442e1c7e95a53413be07
Chris Troutner <chris.troutner@gmail.com>
2017-09-26 14:04:38 -0700

    Adding dashboard component

--- src/components/views/dashboard.vue
+++ src/components/views/dashboard.vue
@@ -102,7 +102,7 @@
           <div class="box-body chat" id="chat-box">
             <!-- chat item -->
             <div class="item">
-              <img src="dist/img/user4-128x128.jpg" alt="user image" class="online">
+              <img src="/static/img/user4-128x128.jpg" alt="user image" class="online">
 
               <p class="message">
                 <a href="#" class="name">
@@ -129,7 +129,7 @@
             <!-- /.item -->
             <!-- chat item -->
             <div class="item">
-              <img src="dist/img/user3-128x128.jpg" alt="user image" class="offline">
+              <img src="/static/img/user3-128x128.jpg" alt="user image" class="offline">
 
               <p class="message">
                 <a href="#" class="name">
@@ -144,7 +144,7 @@
             <!-- /.item -->
             <!-- chat item -->
             <div class="item">
-              <img src="dist/img/user2-160x160.jpg" alt="user image" class="offline">
+              <img src="/static/img/user2-160x160.jpg" alt="user image" class="offline">
 
               <p class="message">
                 <a href="#" class="name">
@@ -190,7 +190,7 @@
           </div>
           <!-- /.box-header -->
           <div class="box-body">
-            <!-- See dist/js/pages/dashboard.js to activate the todoList plugin -->
+            <!-- See /static/js/pages/dashboard.js to activate the todoList plugin -->
             <ul class="todo-list">
               <li>
                 <!-- drag handle -->
