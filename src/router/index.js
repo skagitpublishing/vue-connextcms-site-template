f85495a9585c6389eadb8c75846b8342b131e951
Chris Troutner <chris.troutner@gmail.com>
2017-10-01 08:02:16 -0700

    Importing coPilot scripts to build site template

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,12 @@ export default new Router({
       path: '/',
       name: 'ActiveView',
       component: ActiveView
-    }
+    },
+    {
+      path: '/appdashboard',
+      name: 'ActiveView',
+      component: ActiveView
+    },
+    
   ]
 })
